a683ad515b766628671c703a6579566ef7844248
Emiliano Sanche Juarez <emiliano.sanjua@gmail.com>
2026-04-25 18:50:23 -0600

    Primer commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Reemplaza con tus credenciales de Supabase
-const supabaseUrl = 'https://ulktigzkowfmhuxjjzvh.supabase.co/rest/v1/'
+const supabaseUrl = 'https://ulktigzkowfmhuxjjzvh.supabase.co'
 const supabaseKey = 'sb_publishable_24whM4QqaVjBtQm4WGylew_QH9J5Db8'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -19,3 +19,6 @@ async function obtenerEstudiantes() {
 }
 
 obtenerEstudiantes()
+await supabase
+  .from('estudiantes')
+  .insert([{ nombre: 'Edgar Herrera', carrera: 'ITS' }]);
